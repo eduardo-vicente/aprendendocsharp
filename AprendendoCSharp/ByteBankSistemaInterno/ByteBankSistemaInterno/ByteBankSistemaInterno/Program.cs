32c6cb2ac412dfbb24ad13892902f912a1513b72
Eduardo Tavares Vicente Maria - EVC <eduardo.vicente@iteris.com.br>
2020-01-07 15:53:31 -0200

    adicionando todos projetos no mesmo diretorio

---
+++ AprendendoCSharp/ByteBankSistemaInterno/ByteBankSistemaInterno/ByteBankSistemaInterno/Program.cs
@@ -0,0 +1,20 @@
+﻿using _07_ByteBank;
+using ByteBankRH.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankSistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(12313,1113);
+            Designer desi = new Designer("111.111.113-33");
+
+        }
+    }
+}
