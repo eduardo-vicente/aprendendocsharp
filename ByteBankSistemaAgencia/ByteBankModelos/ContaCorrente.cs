b1236a6b91256f4953b98a90a22b14252aca68ee
Eduardo Tavares Vicente Maria - EVC <eduardo.vicente@iteris.com.br>
2020-01-07 14:19:57 -0200

    criando biblioteca com as classes do outro projeto. aula 01 do modulo 05

---
+++ ByteBankSistemaAgencia/ByteBankModelos/ContaCorrente.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace _07_ByteBank
+{
+    class ContaCorrente
+    {
+
+        public static double TaxaOperacao { get; private set; }
+
+        public static int TotalContasCriadas { get; private set; }
+
+        public Cliente Titular { get; set; }
+
+        public int ContadorSaquesNaoPerimitidos { get; private set; }
+        public int ContadorTransferenciasNaoPermitidas { get; private set; }
+
+        public int Numero { get; }
+        public int Agencia { get; }
+
+
+        private double _saldo = 100;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            if (agencia <= 0)
+            {
+                throw new ArgumentException("Erro de argumento. A agencia tem que ser maior que 0.", nameof(agencia));
+            }
+            if (numero <= 0)
+            {
+                throw new ArgumentException("Erro de argumento. O numero tem que ser maior que 0.", nameof(numero));
+            }
+
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalContasCriadas++;
+            TaxaOperacao = 30 / TotalContasCriadas;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Erro de argumento. Valor de saque não pode ser negativo.", nameof(valor));
+            }
+            if (_saldo < valor)
+            {
+                ContadorSaquesNaoPerimitidos++;
+                throw new SaldoInsuficienteException(Saldo, valor);
+
+            }
+
+            _saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+
+            if (valor < 0)
+            {
+                throw new ArgumentException("Erro de argumento. Valor de transferencia não pode ser negativo.", nameof(valor));
+            }
+            try
+            {
+                Sacar(valor);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+                ContadorTransferenciasNaoPermitidas++;
+                throw new OperacaoFinanceiraException("Operacao nao realizada", ex);
+            }
+
+            contaDestino.Depositar(valor);
+
+        }
+
+    }
+
+}
